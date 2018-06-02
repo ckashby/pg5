dd5cfd0452d8490b237b1204080d4d89504c069a
ckashby <clay.ashby@gmail.com>
2018-06-01 17:12:02 -0700

    Fixed mystery <hr> it was border in css

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,16 +4,19 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get index_url
     assert_response :success
+    assert_select "h1", "Welcome"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
+    assert_select "h1", "About Us"
   end
 
   test "should get contact" do
     get contact_url
     assert_response :success
+    assert_select "h1", "Contact Us"
   end
 
 end
